035db60311241d10dbe444fe4d93114987d1137a
Artem <goldtree3000@gmail.com>
2023-11-17 00:46:38 +0300

    feat: solve countVowels task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -323,8 +323,16 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'];
+  let result = 0;
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (vowels.includes(str[i])) {
+      result += 1;
+    }
+  }
+  return result;
 }
 
 /**
