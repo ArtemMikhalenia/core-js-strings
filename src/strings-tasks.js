0d5d1f1b62d397f0c1d29f61ca02883044caa697
Artem <goldtree3000@gmail.com>
2023-11-17 01:02:21 +0300

    feat: solve isPalindrome task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -348,8 +348,11 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const regExp = /[a-z]/gi;
+  const newString = str.toLowerCase().match(regExp).join('');
+  const reversedString = newString.split('').reverse().join('');
+  return newString === reversedString;
 }
 
 /**
